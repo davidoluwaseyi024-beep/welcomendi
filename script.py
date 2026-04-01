8e8e0322eb662242820e35114b36bb467334fbda
David oluwaseyi <davidoluwaseyi024@gmail.com>
2026-04-01 09:11:57 -0500

    comment

---
+++ script.py
@@ -0,0 +1,12 @@
+#print ("Welcome system")
+
+print ("Welcome NDI Application system")
+name =input("full name:______________")
+age =input("age:___________")
+dob =input("date of birth:___________")
+school =input ("school:___________")
+sex =input("male/female _________")
+
+#print Thank you for applying
+
+print(f"Thank you {name} for applying to NDI Application system we note that your details is saved on our page as {name}, and your age is {age},and you were born on {dob},you attended {school},your gender is {sex}")
